af63fff686db94fae2a4bc9aa18e638ce220c0d5
Sean Eshbaugh <seaneshbaugh@gmail.com>
2023-03-27 22:07:33 -0500

    Initial Commit

---
+++ StayAwake/StayAwakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StayAwakeApp.swift
+//  StayAwake
+//
+//  Created by Sean Eshbaugh on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct StayAwakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
