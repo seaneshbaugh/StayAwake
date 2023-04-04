9f9401d7117c6ba8914a10798b6f80fc8edabd8f
Sean Eshbaugh <seaneshbaugh@gmail.com>
2023-04-04 11:57:24 -0500

    Use MenuBarExtra instead of WindowGroup. Added sleep prevention functionality.

--- StayAwake/ContentView.swift
+++ StayAwake/ContentView.swift
@@ -1,26 +1,48 @@
-//
-//  ContentView.swift
-//  StayAwake
-//
-//  Created by Sean Eshbaugh on 3/27/23.
-//
-
 import SwiftUI
+import IOKit.pwr_mgt
 
 struct ContentView: View {
+    @AppStorage("stayAwake") var stayAwake: Bool = false
+    @Binding var assertionID: IOPMAssertionID
+    @Binding var showErrorAlert: Bool
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button(buttonTitle()) {
+            if stayAwake {
+                IOPMAssertionRelease(assertionID)
+
+                stayAwake = false
+            } else {
+                if IOPMAssertionCreateWithName(kIOPMAssertionTypeNoDisplaySleep as CFString, IOPMAssertionLevel(kIOPMAssertionLevelOn), "StayAwake: Disabling screen sleep" as CFString, &assertionID) == kIOReturnSuccess {
+                    stayAwake = true
+                } else {
+                    showErrorAlert = true
+                }
+            }
         }
-        .padding()
+        .alert("Error preventing screen sleep!", isPresented: $showErrorAlert) {
+            Button("OK", role: .cancel) { }
+        }
+        Divider()
+        Button("Quit") {
+            NSApplication.shared.terminate(nil)
+        }
+    }
+
+    func buttonTitle() -> String {
+        if stayAwake {
+            return "Allow Sleep"
+        } else {
+            return "Prevent Sleep"
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    @State static var assertionID: IOPMAssertionID = 0
+    @State static var showErrorAlert: Bool = false
+
     static var previews: some View {
-        ContentView()
+        ContentView(assertionID: $assertionID, showErrorAlert: $showErrorAlert)
     }
 }
--- StayAwake/StayAwakeApp.swift
+++ StayAwake/StayAwakeApp.swift
@@ -1,17 +1,36 @@
-//
-//  StayAwakeApp.swift
-//  StayAwake
-//
-//  Created by Sean Eshbaugh on 3/27/23.
-//
-
 import SwiftUI
+import IOKit.pwr_mgt
 
 @main
 struct StayAwakeApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+    @AppStorage("stayAwake") var stayAwake: Bool = false
+    @State var assertionID: IOPMAssertionID = 0
+    @State var showErrorAlert: Bool = false
+
+    init() {
+        NSApplication.shared.setActivationPolicy(.accessory)
+
+        if stayAwake {
+            var id: IOPMAssertionID = 0
+
+            if IOPMAssertionCreateWithName(kIOPMAssertionTypeNoDisplaySleep as CFString, IOPMAssertionLevel(kIOPMAssertionLevelOn), "StayAwake: Disabling screen sleep" as CFString, &id) != kIOReturnSuccess {
+                showErrorAlert = true
+            }
+
+            self._assertionID = State(initialValue: id)
         }
+    }
+
+    var body: some Scene {
+        MenuBarExtra {
+            ContentView(assertionID: $assertionID, showErrorAlert: $showErrorAlert)
+        } label: {
+            if stayAwake {
+                Image(systemName: "sun.max")
+            } else {
+                Image(systemName: "sleep")
+            }
+        }
+        .menuBarExtraStyle(.menu)
     }
 }
